67db953c2d432738fe6f5a92df47ac24570cc6b1
zhoudeyou <zhoudeyou@cnaeit.com>
2025-06-16 19:59:45 +0800

    feat: 初始化项目

---
+++ js/prizes.js
@@ -0,0 +1,38 @@
+/**
+ * 奖品列表
+ * name: 奖品名称
+ * count: 奖品数量
+ * stage: 奖品所在轮次
+ */
+const PRIZES_LIST = [
+  {
+    name: '第一轮节目单',
+    count: 8,
+    stage: 1,
+  },
+  {
+    name: '第二轮节目单',
+    count: 8,
+    stage: 2,
+  },
+  {
+    name: '第三轮节目单',
+    count: 8,
+    stage: 3,
+  },
+  {
+    name: '第四轮节目单',
+    count: 8,
+    stage: 4,
+  },
+  {
+    name: '第五轮节目单',
+    count: 8,
+    stage: 5,
+  },
+  {
+    name: '第六轮节目单',
+    count: 8,
+    stage: 6,
+  },
+];
---
+++ js/round.js
@@ -0,0 +1,51 @@
+/**
+ * 表演节目轮次列表
+ * count: 每轮节目数量（固定8个）
+ * stage: 轮次
+ * startNumber: 起始序号
+ * endNumber: 结束序号
+ */
+const PERFORMANCE_ROUNDS = [
+  {
+    count: 8,
+    stage: 1,
+    startNumber: 1,
+    endNumber: 8,
+    title: '第一轮表演'
+  },
+  {
+    count: 8,
+    stage: 2,
+    startNumber: 9,
+    endNumber: 16,
+    title: '第二轮表演'
+  },
+  {
+    count: 8,
+    stage: 3,
+    startNumber: 17,
+    endNumber: 24,
+    title: '第三轮表演'
+  },
+  {
+    count: 8,
+    stage: 4,
+    startNumber: 25,
+    endNumber: 32,
+    title: '第四轮表演'
+  },
+  {
+    count: 8,
+    stage: 5,
+    startNumber: 33,
+    endNumber: 40,
+    title: '第五轮表演'
+  },
+  {
+    count: 8,
+    stage: 6,
+    startNumber: 41,
+    endNumber: 48,
+    title: '第六轮表演'
+  }
+];
